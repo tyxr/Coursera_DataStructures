f51b0944e0e0cafdcbc6774e4bedaca7495044f0
tyxr <751021032@qq.com>
2017-12-28 19:59:19 +0800

    Create merging_tables.py

---
+++ merging_tables.py
@@ -0,0 +1,52 @@
+# python3
+import sys
+
+n, m = map(int, sys.stdin.readline().split())
+lines = list(map(int, sys.stdin.readline().split()))
+rank = [1] * n
+parent = list(range(0, n))
+ans = max(lines)
+act = {}
+
+def getParent(table):
+    if table != parent[table]:
+        parent[table] = getParent(parent[table])
+    return parent[table]
+
+def merge(destination, source):
+    global ans
+    Destination, Source = getParent(destination), getParent(source)
+    lineRoot = 0
+
+    if Destination == Source:
+        return False
+
+    if rank[Destination] > rank[Source]:
+        parent[Source] = Destination
+        lines[Destination] += lines[Source]
+        lineRoot = lines[Destination]
+        lines[Source] = 0
+
+    elif rank[Destination] == rank[Source]:
+        parent[Source] = Destination
+        lines[Destination] += lines[Source]
+        lineRoot = lines[Destination]
+        lines[Source] = 0
+        rank[Destination] += 1
+
+    else:
+        parent[Destination] = Source
+        lines[Source] += lines[Destination]
+        lineRoot = lines[Source]
+        lines[Destination] = 0
+
+    if lineRoot > ans:
+        ans = lineRoot
+
+
+    return True
+
+for i in range(m):
+    destination, source = map(int, sys.stdin.readline().split())
+    merge(destination - 1, source - 1)
+    print(ans)
