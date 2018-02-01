eebf9fc0aa946201b2c4855cc002d7d574e9d487
tyxr <751021032@qq.com>
2018-02-01 18:51:43 +0800

    这个题例子没有一个出错的，上传就不通过。我不知道为什么。

---
+++ hash_chains_pro.py
@@ -0,0 +1,68 @@
+# python3
+
+class Query:
+
+    def __init__(self, query):
+        self.type = query[0]
+        if self.type == 'check':
+            self.ind = int(query[1])
+        else:
+            self.s = query[1]
+
+
+class QueryProcessor:
+    _multiplier = 263
+    _prime = 1000000007
+
+    def __init__(self, bucket_count):
+        self.bucket_count = bucket_count
+        # store all strings in one list
+        self.elems = []
+        self.hashlist = [[] for i in range(5)]
+
+    def _hash_func(self, s):
+        ans = 0
+        for c in reversed(s):
+            ans = (ans * self._multiplier + ord(c)) % self._prime
+        return ans % self.bucket_count
+
+    def write_search_result(self, was_found):
+        print('yes' if was_found else 'no')
+
+    def write_chain(self, chain):
+        
+        chain.reverse()
+        print(' '.join(chain))
+
+    def read_query(self):
+        return Query(input().split())
+
+    def process_query(self, query):
+        if query.type == "check":
+            self.write_chain(self.hashlist[query.ind])
+        else:
+            
+            hash_value = self._hash_func(query.s)
+            if query.s in self.hashlist[hash_value]:
+                ind = hash_value 
+            else:
+                ind = -1
+            if query.type == 'find':
+                self.write_search_result(ind != -1)
+            elif query.type == 'add':
+                if ind == -1:
+                    self.hashlist[hash_value].append(query.s)
+            else:
+                if ind != -1:
+                    self.hashlist[hash_value].remove(query.s)
+        
+
+    def process_queries(self):
+        n = int(input())
+        for i in range(n):
+            self.process_query(self.read_query())
+
+if __name__ == '__main__':
+    bucket_count = int(input())
+    proc = QueryProcessor(bucket_count)
+    proc.process_queries()
